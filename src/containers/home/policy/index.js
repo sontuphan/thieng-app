203c25eee5be2c336990e3079a6f53d19f0f9cc9
tphanson <tphanson@kambria.io>
2020-01-08 17:59:13 +0700

    more btn

--- src/containers/home/policy/index.js
+++ src/containers/home/policy/index.js
@@ -6,6 +6,9 @@ import { withRouter } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+import { ArrowForwardIos } from '@material-ui/icons';
 
 import Drain from 'components/drain';
 import Carousel from 'components/carousel';
@@ -41,6 +44,11 @@ class Policy extends Component {
           </Grid>
           <Grid item xs={10} md={8}>
             <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet turpis sed gravida amet. Luctus sed parturient lacus vestibulum nisl neque. Vehicula risus tellus viverra cursus et. Porta arcu tincidunt enim ut platea in amet, at. Aliquet risus sem arcu pretium rutrum. Sit enim nec viverra sapien semper imperdiet. A cursus.</Typography>
+          </Grid>
+          <Grid item xs={10} md={8}>
+            <Button variant="outlined" color="primary" size="large" endIcon={<ArrowForwardIos />}>
+              <Typography>Nhiều hơn</Typography>
+            </Button>
           </Grid>
           <Drain />
         </Grid>
