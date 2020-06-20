624211f5bb052d4f0a4f81951e2fcbca38c1a721
tphanson <tphanson@kambria.io>
2020-06-20 16:23:37 +0700

    change icons

--- src/configs/category.config.js
+++ src/configs/category.config.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { FaChair, FaTableTennis, FaTree, FaDice } from 'react-icons/fa';
-import { GiDesk, GiBedLamp, GiCookingPot, GiCeilingLight } from 'react-icons/gi';
+import { FaTableTennis, FaTree, FaDice } from 'react-icons/fa';
+import { GiRockingChair, GiDesk, GiBedLamp, GiCookingPot, GiCeilingLight } from 'react-icons/gi';
 import { MdTexture } from 'react-icons/md';
 import { TiSortAlphabetically } from 'react-icons/ti';
 
@@ -11,13 +11,13 @@ import { TiSortAlphabetically } from 'react-icons/ti';
 const configs = {}
 
 const pureList = [
-  { value: 'chairs', name: 'Ghế', icon: <FaChair /> },
+  { value: 'chairs', name: 'Ghế', icon: <GiRockingChair /> },
   { value: 'desks', name: 'Bàn', icon: <GiDesk /> },
-  { value: 'floor', name: 'Sàn gỗ', icon: <MdTexture /> },
+  { value: 'kitchen', name: 'Nhà bếp', icon: <GiCookingPot /> },
   { value: 'light', name: 'Đèn', icon: <GiCeilingLight /> },
+  { value: 'floor', name: 'Sàn gỗ', icon: <MdTexture /> },
   { value: 'bedroom', name: 'Phòng ngủ', icon: <GiBedLamp /> },
   { value: 'playground', name: 'Sân chơi', icon: <FaTableTennis /> },
-  { value: 'kitchen', name: 'Nhà bếp', icon: <GiCookingPot /> },
   { value: 'garden', name: 'Vườn', icon: <FaTree /> },
   { value: 'others', name: 'Khác', icon: <FaDice /> },
 ]
