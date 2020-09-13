1e7b581aadb4acc86ddd92a29d5750d36d2fde98
tphanson <tphanson@kambria.io>
2020-09-13 23:12:03 +0700

    bugfix

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,6 +1,5 @@
 import StatusCard from './status';
 import ImageCard from './image';
-import PortraitCard from './portrait';
 import ProductCard from './product';
 import NotiCard from './noti';
 import EventCard from './event';
@@ -8,7 +7,6 @@ import UserCard from './user';
 import GalleryCard from './gallery';
 
 export {
-  StatusCard, ImageCard, PortraitCard,
-  ProductCard, NotiCard, EventCard,
-  UserCard, GalleryCard,
+  StatusCard, ImageCard, ProductCard,
+  NotiCard, EventCard, UserCard, GalleryCard,
 }
