55b6a31ac904f0c0088fbdc3a10b90758e18edd4
tphanson <tphanson@kambria.io>
2020-09-27 12:28:17 +0700

    rich cards

--- src/components/cards/item/rich/index.js
+++ src/components/cards/item/rich/index.js
@@ -4,40 +4,57 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 
+import { useTheme } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import Divider from '@material-ui/core/Divider';
 
-import { } from '@material-ui/icons';
+import {
+  ExpandLessRounded, ExpandMoreRounded, ShoppingCartRounded,
+  ShareRounded, RemoveShoppingCartRounded
+} from '@material-ui/icons';
 
 import { BaseCard, ImageCard } from 'components/cards';
 import { NumericInput } from 'components/inputs';
 import { UserCard } from 'components/cards';
 
-import { setCart } from 'modules/cart.reducer';
+import { setCart, toggleCart } from 'modules/cart.reducer';
 import { useStyles } from './styles';
 import { useData } from './module';
 import utils from 'helpers/utils';
 
 function RichItemCard(props) {
   // Define hooks
+  const [checked, setChecked] = useState(false);
+  const [visible, setVisible] = useState(false);
   const [amount, setAmount] = useState(props.amount);
+  const theme = useTheme();
   const classes = useStyles();
   const data = useData(props.itemId);
-  // Define internal functions
-  const useBuy = () => {
+  // Define functions
+  const onBuy = () => {
     if (!amount) return console.error('Invalid amount');
-    return props.setCart({ _id: data._id, amount });
+    return props.setCart({ ...data, amount });
   }
-  const useCancel = () => {
-    return props.setCart({ _id: data._id, amount: 0 }).then(() => {
+  const onCancel = () => {
+    return props.setCart({ ...data, amount: 0 }).then(() => {
       return setAmount(1);
     }).catch(console.error);
   }
-  // Define functions
-  if (!data || data instanceof Error) return null;
+  const onCollapse = () => setChecked((prev) => !prev);
+  const onVisible = () => setVisible((prev) => !prev);
+  const isInCart = () => {
+    if (!data) return false;
+    const { cart: { data: carts } } = props;
+    return carts.map(({ _id }) => _id).includes(data._id);
+  }
   // Render component
+  if (!data || data instanceof Error) return null;
   return <Grid container spacing={2}>
     <Grid item xs={12}>
       <BaseCard>
@@ -67,7 +84,7 @@ function RichItemCard(props) {
               <Grid item xs={12}>
                 <Grid container spacing={1} alignItems="center" justify="flex-end">
                   <Grid item>
-                    <Typography>{utils.prettyNumber(data.price * amount, 'long')} ₫</Typography>
+                    <Typography variant="body2">{utils.prettyNumber(data.price * amount, 'long')} ₫</Typography>
                   </Grid>
                 </Grid>
               </Grid>
@@ -75,24 +92,50 @@ function RichItemCard(props) {
                 <NumericInput variant="outlined" size="small" value={amount} onChange={setAmount} />
               </Grid>
               <Grid item xs={6}>
-                <Button variant="contained" color="primary" size="small" onClick={useBuy} fullWidth >
+                <Button variant="contained" color="primary" size="small" onClick={onBuy} fullWidth >
                   <Typography>Mua</Typography>
                 </Button>
               </Grid>
               <Grid item xs={6}>
-                <Button variant="contained" color="secondary" size="small" onClick={useCancel} fullWidth  >
+                <Button variant="contained" color="secondary" size="small" onClick={onCancel} fullWidth  >
                   <Typography>Hủy</Typography>
                 </Button>
               </Grid>
             </Grid>
           </Grid>
+          <Collapse in={checked} collapsedHeight={0}>
+            <Grid item xs={12} style={{ padding: theme.spacing(1) }}>
+              {props.body ? props.body : data.descriptions[0]}
+            </Grid>
+          </Collapse>
           <Grid item xs={12}>
-            <Typography>{data.descriptions[0]}</Typography>
+            <Divider />
           </Grid>
-          <Grid item xs={12}>
-            {props.body}
+          <Grid item xs={12} className={classes.action}>
+            <Grid container spaing={2} justify="space-between">
+              <Grid item>
+                <Tooltip title="Giỏ hàng">
+                  <IconButton size="small" onClick={props.toggleCart}>
+                    {isInCart() ? <ShoppingCartRounded fontSize="small" color="primary" /> : <RemoveShoppingCartRounded fontSize="small" />}
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Chi tiết">
+                  <IconButton size="small" onClick={onCollapse}>
+                    {checked ? <ExpandLessRounded fontSize="small" /> : <ExpandMoreRounded fontSize="small" />}
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Chia sẻ">
+                  <IconButton size="small" onClick={onVisible}>
+                    <ShareRounded fontSize="small" color={visible ? 'primary' : 'inherit'} />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={12} /> {/* Safe zone trick*/}
         </Grid>
       </BaseCard>
     </Grid>
@@ -111,11 +154,11 @@ RichItemCard.propTypes = {
 }
 
 const mapStateToProps = state => ({
-
+  cart: state.cart,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  setCart,
+  setCart, toggleCart,
 }, dispatch);
 
 export default connect(
--- src/components/cards/item/rich/styles.js
+++ src/components/cards/item/rich/styles.js
@@ -1,10 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
-  cursor:{
+  cursor: {
     cursor: 'pointer',
   },
-  collapse: {
-    borderRadius: theme.shape.borderRadius,
+  action: {
+    padding: `${theme.spacing(0)}px ${theme.spacing(1)}px !important`
   },
 }));
